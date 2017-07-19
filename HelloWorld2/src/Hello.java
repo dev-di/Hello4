e1cd47427bb1ff814294841c67d75e8a32907a28
dev-di <admin@masterin.se>
2017-07-19 12:14:25 +0200

    Message edited

--- HelloWorld2/src/Hello.java
+++ HelloWorld2/src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 
 	public static void main(String [] args) {
-		System.out.println("Tjena igen!!!!!!");
+		System.out.println("Tjena!");
 	}
 	
 	public int add(int i1, int i2) {
