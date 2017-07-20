cc59bff04bfecde4746060c3e866f345064322ff
dev-di <admin@masterin.se>
2017-07-20 12:14:44 +0200

    initial commit for hello4 project

---
+++ HelloWorld4/src/main/java/hello4/Hello.java
@@ -0,0 +1,10 @@
+package hello4;
+
+public class Hello {
+	public static void main(String [] args) {
+		System.out.println("Hello World!");
+	}
+	public String getMessage() {
+		return "Hello World!";
+	}
+}
